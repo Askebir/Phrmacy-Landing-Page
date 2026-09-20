7e3de1601ecd7141e19bc3f84cbd627aa4ea7ae8
Askebir Tadesse <askebirtadesse@gmail.com>
2026-09-20 23:29:11 +0300

    product section complated

--- components/home/Products.tsx
+++ components/home/Products.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import LightText from "../subComponents/LightText";
 
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <section id="products">
+      <div>
+        <LightText text="Product Catalog" />
+        <div>
+          <h1>Explore Our Products</h1>
+        </div>
+        <div className="text-black/60">
+          <p>Find the healthcare and personal care products you need in one </p>
+          <div className="flex justify-center">convenient place.</div>
+        </div>
+      </div>
+    </section>
+  );
 }
